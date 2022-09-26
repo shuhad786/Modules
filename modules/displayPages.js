040384de066a3912ca57c9e469bfe28e1f6a454f
Shuhad786 <shuhad.loofer@gmail.com>
2022-09-26 11:33:34 +0200

    Updated displayPages.js for linter errors

--- modules/displayPages.js
+++ modules/displayPages.js
@@ -1,22 +1,22 @@
 const ContactPage = () => {
   document.getElementById('contactLink').addEventListener('click', () => {
-  document.getElementById('addPage').classList = 'displayNone';
-  document.getElementById('listPage').classList = 'displayNone';
-  document.getElementById('contactPage').classList = 'displayBlock';
-})};
+    document.getElementById('addPage').classList = 'displayNone';
+    document.getElementById('listPage').classList = 'displayNone';
+    document.getElementById('contactPage').classList = 'displayBlock';
+  }) };
 
 const ListPage = () => {
   document.getElementById('listLink').addEventListener('click', () => {
-  document.getElementById('addPage').classList = 'displayNone';
-  document.getElementById('listPage').classList = 'displayBlock';
-  document.getElementById('contactPage').classList = 'displayNone';
-})};
+    document.getElementById('addPage').classList = 'displayNone';
+    document.getElementById('listPage').classList = 'displayBlock';
+    document.getElementById('contactPage').classList = 'displayNone';
+  }) };
 
 const AddPage = () => {
   document.getElementById('addLink').addEventListener('click', () => {
-  document.getElementById('addPage').classList = 'displayBlock';
-  document.getElementById('listPage').classList = 'displayNone';
-  document.getElementById('contactPage').classList = 'displayNone';
-})};
+    document.getElementById('addPage').classList = 'displayBlock';
+    document.getElementById('listPage').classList = 'displayNone';
+    document.getElementById('contactPage').classList = 'displayNone';
+  }) };
 
 export { ListPage, AddPage, ContactPage };

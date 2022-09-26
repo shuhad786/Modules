c49aa0e5af9d29d279c556787ba93e271a8d4aef
Shuhad786 <shuhad.loofer@gmail.com>
2022-09-26 11:58:33 +0200

    eslint

--- modules/displayPages.js
+++ modules/displayPages.js
@@ -1,24 +1,24 @@
 const ContactPage = () => {
-  document.getElementById('contactLink').addEventListener('click', () => {
-    document.getElementById('addPage').classList = 'displayNone';
-    document.getElementById('listPage').classList = 'displayNone';
-    document.getElementById('contactPage').classList = 'displayBlock';
+  document.getElementById("contactLink").addEventListener("click", () => {
+    document.getElementById("addPage").classList = "displayNone";
+    document.getElementById("listPage").classList = "displayNone";
+    document.getElementById("contactPage").classList = "displayBlock";
   });
 };
 
 const ListPage = () => {
-  document.getElementById('listLink').addEventListener('click', () => {
-    document.getElementById('addPage').classList = 'displayNone';
-    document.getElementById('listPage').classList = 'displayBlock';
-    document.getElementById('contactPage').classList = 'displayNone';
+  document.getElementById("listLink").addEventListener("click", () => {
+    document.getElementById("addPage").classList = "displayNone";
+    document.getElementById("listPage").classList = "displayBlock";
+    document.getElementById("contactPage").classList = "displayNone";
   });
 };
 
 const AddPage = () => {
-  document.getElementById('addLink').addEventListener('click', () => {
-    document.getElementById('addPage').classList = 'displayBlock';
-    document.getElementById('listPage').classList = 'displayNone';
-    document.getElementById('contactPage').classList = 'displayNone';
+  document.getElementById("addLink").addEventListener("click", () => {
+    document.getElementById("addPage").classList = "displayBlock";
+    document.getElementById("listPage").classList = "displayNone";
+    document.getElementById("contactPage").classList = "displayNone";
   });
 };
 
